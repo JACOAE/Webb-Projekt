2d05b960352b57ec2134445f53c4107dc94db64c
John <engblomsandinjohn@gmail.com>
2021-10-18 15:34:11 +0200

    SPorts och teams kar

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,48 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-                {/* TODO */}
-            </button>
-        );
-    }
-}
-
-class Board extends React.Component {
-    renderSquare(i) {
-        return <Square />;
-    }
-
-    render() {
-        const status = 'Next player: X';
-
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
-
-
 
 class GetTeams extends React.Component {
     constructor(props) {
@@ -86,11 +44,64 @@ class GetTeams extends React.Component {
         } else {
             return (
                 <ul>
-                    {teams.map(team => (
-                        <li key={team.id}>
-                            {team.name}
-                        </li>
-                    ))}
+                {teams.map(team => (
+                    <li key={team.id}>
+                      {team.name}
+                    </li>
+                  ))}
+                </ul>
+            );
+        }
+    }
+
+}
+
+class GetSports extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://api.everysport.com/v1/sports?apikey=26192887ec48f76ab54167238ae16688")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        sports: result.sports
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, sports } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+                {sports.map(sport => (
+                    <li key={sport.id}>
+                      {sport.name}
+                    </li>
+                  ))}
                 </ul>
             );
         }
@@ -126,7 +137,7 @@ class Site extends React.Component {
 class Menu extends React.Component {
     render() {
         return (
-            <GetTeams />
+            <GetSports />
         );
     }
 }
