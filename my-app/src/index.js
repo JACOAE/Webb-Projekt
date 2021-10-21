3ff71b3e1ecc4b8f9eb5d6e34b749e33e73d7f43
John <engblomsandinjohn@gmail.com>
2021-10-21 13:23:31 +0200

    Bwasdmngsdg

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,49 +5,73 @@ import Logo from './img/logoB.png';
 
 var sportIDChosen = 0;
 var teamIDChosen = 0;
+var teamInfoElement;
 
 
 class GetTeamInfo extends React.Component {
-    constructor(props,sportid) {
+    constructor(props) {
         super(props);
+       
+
         this.state = {
+            statement: this.props.statement,
+
             error: null,
             isLoaded: false,
-            items: []
+            items: [],
+            teamID: this.props.teamID
         };
     }
 
-    componentDidMount() {
-        fetch("http://api.everysport.com/v1/teams/"+teamIDChosen+"?apikey=26192887ec48f76ab54167238ae16688")      
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        team: result.team
-                    });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
+    
+
+    setTeamId(teamID) {
+        this.teamID = teamID;
     }
+
+
+    componentDidMount() {
+        if (this.teamIDChosen == 0) {
+
+        }
+        else {
+            fetch("http://api.everysport.com/v1/teams/" + teamIDChosen + "?apikey=26192887ec48f76ab54167238ae16688")
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        this.setState({
+                            isLoaded: true,
+                            team: result.team
+
+                        });
+
+                    },
+                    // Note: it's important to handle errors here
+                    // instead of a catch() block so that we don't swallow
+                    // exceptions from actual bugs in components.
+                    (error) => {
+                        this.setState({
+                            isLoaded: true,
+                            error
+                        });
+                    }
+                )
+        }
+    }
+
 
     render() {
         const { error, isLoaded, team } = this.state;
         if (error) {
             return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
-            return <div>Loading...</div>;
+            return <div>{this.state.statement}</div>;
         } else {
-            return (    
-                team.name
+            return (
+                <GetTeams setTeamId ={this.setTeamId} />
+
+
+                //team.name
             );
         }
     }
@@ -55,7 +79,7 @@ class GetTeamInfo extends React.Component {
 }
 
 class GetTeams extends React.Component {
-    constructor(props,sportid) {
+    constructor(props, sportid) {
         super(props);
         this.state = {
             error: null,
@@ -65,7 +89,7 @@ class GetTeams extends React.Component {
     }
 
     componentDidMount() {
-        fetch("http://api.everysport.com/v1/teams?apikey=26192887ec48f76ab54167238ae16688"+"&sport="+sportIDChosen)      
+        fetch("http://api.everysport.com/v1/teams?apikey=26192887ec48f76ab54167238ae16688" + "&sport=" + sportIDChosen)
             .then(res => res.json())
             .then(
                 (result) => {
@@ -96,11 +120,11 @@ class GetTeams extends React.Component {
             return (
                 <ul>
                     <h2>Välj ett lag:</h2>
-                {teams.map(team => (
-                    <a onClick={()=> clickTeams(team.id)}><li key={team.id}>
-                    {team.name}
-                  </li></a>
-                  ))}
+                    {teams.map(team => (
+                        <a onClick={() => this.props.setTeamId(team.id)}><li key={team.id}>
+                            {team.name}
+                        </li></a>
+                    ))};
                 </ul>
             );
         }
@@ -150,11 +174,11 @@ class GetSports extends React.Component {
             return (
                 <ul>
                     <h2>Välj en sport:</h2>
-                {sports.map(sport => ( //Skriver ut sporten och kör funktionen clickSports när list itemet klickas på
-                    <a onClick={()=> clickSports(sport.id)}><li key={sport.id}>
-                      {sport.name}
-                    </li></a>
-                  ))}
+                    {sports.map(sport => ( //Skriver ut sporten och kör funktionen clickSports när list itemet klickas på
+                        <a onClick={() => clickSports(sport.id)}><li key={sport.id}>
+                            {sport.name}
+                        </li></a>
+                    ))}
                 </ul>
             );
         }
@@ -169,11 +193,13 @@ function clickSports(sportid) {
     ReactDOM.render(element, document.getElementById('menu_container'));
 }
 
-function clickTeams(teamid){
-    alert(teamid + "clicked!")
+function clickTeams(teamid) {
+    alert(teamid + " clicked!")
     teamIDChosen = teamid;
-    var element = <GetTeamInfo />
-    ReactDOM.render(element, document.getElementById("main"));
+    teamInfoElement = <GetTeamInfo teamID = {teamIDChosen}/>
+
+    
+    ReactDOM.render(teamInfoElement, document.getElementById("main"));
 }
 
 
@@ -191,7 +217,7 @@ class Site extends React.Component {
                 </div>
 
                 <div id="main">
-                    <Main />
+                    <GetTeamInfo statement='Hello'/>
                 </div>
 
                 <div id="footer">
@@ -212,7 +238,7 @@ class Menu extends React.Component {
 
 class Main extends React.Component {
     render() {
-        return(
+        return (
             <h3>Välkommen till MonkeySports!</h3>
         )
     }
